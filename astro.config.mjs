ad08efa555c9579b83b3737e235d285802899084
jlooper <jen.looper@gmail.com>
2024-04-14 16:50:44 -0400

    optimizing for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import icon from "astro-icon";
-
 import vue from "@astrojs/vue";
+import netlify from "@astrojs/netlify"; 
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false
-  }), compress(), vue()]
+  }), compress(), vue()],
+  output: "server",
+  adapter: netlify()
 });
